92cd9a678b3779ff3d9e22b448d272efff7de9fe
Nathan COLIN <nathan.colin-ext@relyens.eu>
2024-03-06 17:30:26 +0100

    Minor clean up & comments

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Doesn't work as a standalone jar yet
 - Place files in target folder (fr, en and es)
 - Start service
 - See that a .fr.resx.translated has been generated with all translations in it
+
+## Problems
+- If it fails, nothing is written, your deepl's character count doesn't go down --> Should physically write results as soon as its translated instead of in the inputstream. This would allow you to see progress and protect you against unforeseen errors
+- Didn't take the time to properly make this a service. You have to start the project to make it run as the translation happen in PostConstruct of the XmlParser... (yup)
--- src/main/java/fr/ncolin/development/xmltranslator/service/XmlParser.java
+++ src/main/java/fr/ncolin/development/xmltranslator/service/XmlParser.java
@@ -13,7 +13,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
@@ -44,21 +43,21 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class XmlParser {
 
+	// number of labels to translate in a single deepl call
 	public static final int BATCH_SIZE = 100;
 	private static final String FORMAT_XSLT = "src/main/resources/format.xslt";
+	private static final String KEY = "#KEY#";
 
 	private final WebClient webClient;
 	private final DeeplProperties deeplProperties;
@@ -68,11 +67,134 @@ public class XmlParser {
 		this.findAllElementToTranslate();
 	}
 
-	public List<String> findAllElementToTranslate() {
+	public void findAllElementToTranslate() {
+		// 1. get all files
 		List<Path> files = this.getFiles();
-		Map<String, List<String>> toTranslateByFile = new HashMap<>();
+
+		// 2. For each french file, find all the codes needing a translation. Ex:
+		// All_Resources.fr.resx: [Action_CycleCountSchedule_ActivateAutocreate, Action_CycleCountSchedule_CancelIteration, ...]
+		Map<String, List<String>> toTranslateByFile = this.extractLabelsToTranslateByFiles(files);
+
+		// Stats & making sure everything is accounted for
+		final AtomicLong nbTranslatedEnglish = new AtomicLong(0);
+		final AtomicLong nbTranslatedSpanish = new AtomicLong(0);
+		final AtomicLong nbDirectlyTransmitted = new AtomicLong(0);
+		final AtomicLong total = new AtomicLong(0);
+
+		// 3. Translate file by file
+		toTranslateByFile
+				.forEach((filename, labels) -> translateForFile(filename, labels, total, nbTranslatedEnglish, nbTranslatedSpanish, nbDirectlyTransmitted));
+
+		System.out.println("Total: " + total);
+		System.out.println("EN: " + nbTranslatedEnglish.get());
+		System.out.println("ES: " + nbTranslatedSpanish.get());
+		System.out.println("KEY: " + nbDirectlyTransmitted.get());
+	}
+
+	private void translateForFile(String fileName, List<String> labelsToTranslate, AtomicLong total, AtomicLong nbTranslatedEnglish, AtomicLong nbTranslatedSpanish, AtomicLong nbDirectlyTransmitted) {
+		// 1. Separates labels to translate from different languages or not translate at all
+		final Map<String, String> toTranslateEn = new HashMap<>();
+		final Map<String, String> toTranslateEs = new HashMap<>();
+		final Map<String, String> toSetDirectly = new HashMap<>();
+
+		// 2. Init deepl's translation request
+		TranslationRequest requestEn = new TranslationRequest();
+		requestEn.setTarget_lang("FR");
+		requestEn.setSource_lang("EN");
+		requestEn.setText(new ArrayList<>());
+
+		TranslationRequest requestEs = new TranslationRequest();
+		requestEs.setTarget_lang("FR");
+		requestEs.setSource_lang("ES");
+		requestEs.setText(new ArrayList<>());
+
+		// 3. Open stream on the French file
+		try (InputStream is = new FileInputStream(fileName)) {
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			Document translatedDoc = builder.parse(is);
+
+			System.out.println("Total to translate for file " + fileName + ": " + labelsToTranslate.size());
+			for (int i = 0; i < labelsToTranslate.size(); i++) {
+				total.incrementAndGet();
+
+				// 3a. Find a base to translate in other languages
+				this.findInOtherFiles(fileName, labelsToTranslate.get(i), toTranslateEs, toTranslateEn, toSetDirectly);
+
+				if (i % BATCH_SIZE == 0) {
+					// prints cursor every BATCH_SIZE
+					System.out.println(i);
+				}
+
+				// 3b. When toTranslate list gets to BATCH_SIZE elements, translates the elements and sets them in virtual file
+				if (toTranslateEn.size() == BATCH_SIZE) {
+					System.out.println("translating english batch");
+					System.out.println(i);
+					translateAndWrite(nbTranslatedEnglish, requestEn, toTranslateEn, translatedDoc);
+				}
+				if (toTranslateEs.size() == BATCH_SIZE) {
+					System.out.println("translating spanish batch");
+					System.out.println(i);
+					translateAndWrite(nbTranslatedSpanish, requestEs, toTranslateEs, translatedDoc);
+				}
+
+				// debatable if this is necessary... BATCH_SIZE is mainly there to limit http call size to deepl
+				if (toSetDirectly.size() == BATCH_SIZE) {
+					System.out.println("filling direct batch");
+					System.out.println(i);
+					nbDirectlyTransmitted.addAndGet(BATCH_SIZE);
+					toSetDirectly.forEach((mapKey, mapValue) -> this.setInXml(translatedDoc, mapKey, mapValue));
+					toSetDirectly.clear();
+				}
+			}
+
+			// 4. Translated the rest when we're finished with the label finding
+			System.out.println("translating last english batch");
+			translateAndWrite(nbTranslatedEnglish, requestEn, toTranslateEn, translatedDoc);
+
+			System.out.println("translating spanish batch");
+			translateAndWrite(nbTranslatedSpanish, requestEs, toTranslateEs, translatedDoc);
+
+			System.out.println("inserting not translated fields");
+			nbDirectlyTransmitted.addAndGet(toSetDirectly.size());
+			toSetDirectly.forEach((mapKey, mapValue) -> this.setInXml(translatedDoc, mapKey, mapValue));
+
+			try (FileOutputStream output =
+						 new FileOutputStream(fileName + ".translated")) {
+				writeXml(translatedDoc, output);
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Calls Deepl then write result in file
+	 *
+	 * @param nbTranslated counter to increment
+	 * @param request deepl request
+	 * @param toTranslate Map<Name,Value>
+	 * @param translatedDoc doc to update
+	 */
+	private void translateAndWrite(AtomicLong nbTranslated, TranslationRequest request, Map<String, String> toTranslate, Document translatedDoc) {
+		nbTranslated.addAndGet(toTranslate.size());
+		request.setText(toTranslate.values().stream().toList());
+		List<String> translatedEn = this.getTranslations(request);
+		for (int j = 0; j < toTranslate.size(); j++) {
+			this.setInXml(translatedDoc, toTranslate.keySet().stream().toList().get(j), translatedEn.get(j));
+		}
+		toTranslate.clear();
+	}
+
+	/**
+	 * For each file, extracts label list that have null value. Matches them with filename
+	 *
+	 * @param files target file
+	 * @return Map<path_to_file, [name1, name2, ...]>
+	 */
+	private Map<String, List<String>> extractLabelsToTranslateByFiles(List<Path> files) {
+		final Map<String, List<String>> toTranslateByFile = new HashMap<>();
 		files.stream()
-				//.peek(System.out::println)
 				.filter(path -> path.toString().contains(".fr."))
 				.map(path -> {
 					try {
@@ -82,93 +204,65 @@ public class XmlParser {
 					}
 				})
 				.filter(Objects::nonNull)
-				.forEach(tuple -> {
-					toTranslateByFile.put(tuple.getT1().toString(), new ArrayList<>());
-
-					try {
-						DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-						DocumentBuilder builder = builderFactory.newDocumentBuilder();
-						Document xmlDocument = builder.parse(tuple.getT2());
-						String expression = "/root/data";
-						XPath xPath = XPathFactory.newInstance().newXPath();
-						NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-						for (int i = 0; i < nodeList.getLength(); i++) {
-							Node node = nodeList.item(i);
-							for (int j = 0; j < node.getChildNodes().getLength(); j++) {
-								Node child = node.getChildNodes().item(j);
-								if (Node.ELEMENT_NODE == child.getNodeType() && (null == child.getTextContent() || child.getTextContent().isBlank())) {
-									String dataName = this.getDataName(node);
-									toTranslateByFile.get(tuple.getT1().toString()).add(dataName);
-									break;
-								}
-							}
-						}
-					} catch (XPathExpressionException | ParserConfigurationException | IOException | SAXException e) {
-						System.err.println("error: " + e.getMessage());
-					}
-				});
-
-		toTranslateByFile
-				.forEach((key, value) -> {
-					final Map<String, String> toTranslateEn = new HashMap<>();
-					final Map<String, String> toTranslateEs = new HashMap<>();
-					TranslationRequest requestEn = new TranslationRequest();
-					requestEn.setTarget_lang("FR");
-					requestEn.setSource_lang("EN");
-					requestEn.setText(new ArrayList<>());
-					TranslationRequest requestEs = new TranslationRequest();
-					requestEs.setTarget_lang("FR");
-					requestEs.setSource_lang("ES");
-					requestEs.setText(new ArrayList<>());
-					try (InputStream is = new FileInputStream(key)) {
-						DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-						DocumentBuilder builder = builderFactory.newDocumentBuilder();
-						Document translatedDoc = builder.parse(is);
-						for (int i = 0; i < value.size(); i++) {
-							this.find(key, value.get(i), toTranslateEs, toTranslateEn);
-
-							if (toTranslateEn.size() == BATCH_SIZE) {
-								requestEn.setText(toTranslateEn.values().stream().toList());
-								List<String> translatedEn = this.getTranslations(requestEn);
-								for (int j = 0; j < toTranslateEn.values().stream().toList().size(); j++) {
-									this.setInXml(translatedDoc, toTranslateEn.keySet().stream().toList().get(j), translatedEn.get(j));
-								}
-								toTranslateEn.clear();
-							}
-							if (toTranslateEs.size() == BATCH_SIZE) {
-								requestEs.setText(toTranslateEs.values().stream().toList());
-								List<String> translatedEs = this.getTranslations(requestEs);
-								for (int j = 0; j < toTranslateEs.values().stream().toList().size(); j++) {
-									this.setInXml(translatedDoc, toTranslateEs.keySet().stream().toList().get(j), translatedEs.get(j));
-								}
-								toTranslateEs.clear();
-							}
-						}
-						requestEn.setText(toTranslateEn.values().stream().toList());
-						List<String> translatedEn = this.getTranslations(requestEn);
-						for (int j = 0; j < toTranslateEn.values().stream().toList().size(); j++) {
-							this.setInXml(translatedDoc, toTranslateEn.keySet().stream().toList().get(j), translatedEn.get(j));
-						}
-
-						requestEs.setText(toTranslateEs.values().stream().toList());
-						List<String> translatedEs = this.getTranslations(requestEs);
-						for (int j = 0; j < toTranslateEs.values().stream().toList().size(); j++) {
-							this.setInXml(translatedDoc, toTranslateEs.keySet().stream().toList().get(j), translatedEs.get(j));
-						}
-						try (FileOutputStream output =
-									 new FileOutputStream(key + ".translated")) {
-							writeXml(translatedDoc, output);
-						}
-					} catch (Exception e) {
-						System.err.println(e.getMessage());
-					}
-
-				});
-
-		return new ArrayList<>();
+				.forEach(tuple -> toTranslateByFile.put(tuple.getT1().toString(), extractLabelToTranslateFromFile(tuple)));
+		return toTranslateByFile;
 	}
 
-	private void find(String path, String name, final Map<String, String> es, final Map<String, String> en) {
+	/**
+	 * For one file (represented by a tuple<path_to_file, inputstream>), extracts all labels that have a null value
+	 *
+	 * @param file target file
+	 * @return list of labels
+	 */
+	private List<String> extractLabelToTranslateFromFile(Tuple2<Path, InputStream> file) {
+		List<String> labelsToTranslate = new ArrayList<>();
+		try {
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			Document xmlDocument = builder.parse(file.getT2());
+			// finds all node which path matches /root/data
+			String expression = "/root/data";
+			XPath xPath = XPathFactory.newInstance().newXPath();
+			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+			// loop through all data nodes
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node node = nodeList.item(i);
+				// loop through all nodes inside data node  - should be only one element node: value + other nodes that we skip
+				for (int j = 0; j < node.getChildNodes().getLength(); j++) {
+					Node child = node.getChildNodes().item(j);
+					if (Node.ELEMENT_NODE == child.getNodeType() && (null == child.getTextContent() || child.getTextContent().isBlank())) {
+						String dataName = this.getDataName(node);
+						labelsToTranslate.add(dataName);
+						break;
+					}
+				}
+			}
+		} catch (XPathExpressionException | ParserConfigurationException | IOException | SAXException e) {
+			System.err.println("error: " + e.getMessage());
+		}
+		return labelsToTranslate;
+	}
+
+	/**
+	 * Searches other files for a base version of the name we could translate.
+	 * if present in spanish:
+	 * 		if contains #KEY#:
+	 * 			adds to direct list
+	 * 		else
+	 * 			add to spanish list
+	 * 	else if present in english:
+	 * 		if contains #KEY#:
+	 * 			adds to direct list
+	 * 		else
+	 * 			add to english list
+	 *
+	 * @param path filename
+	 * @param name name of label
+	 * @param es spanishTranslations
+	 * @param en englishTranslations
+	 * @param toSetDirectly dontTranslate
+	 */
+	private void findInOtherFiles(String path, String name, final Map<String, String> es, final Map<String, String> en, final Map<String, String> toSetDirectly) {
 		try {
 			String[] split = path.split("\\.");
 			String spanishFileName = split[0] + ".es." + split[2];
@@ -181,12 +275,19 @@ public class XmlParser {
 			XPath spanishXpath = XPathFactory.newInstance().newXPath();
 			NodeList spanishNodeList = (NodeList) spanishXpath.compile(find).evaluate(spanishDocument, XPathConstants.NODESET);
 
-			if (0 == spanishNodeList.getLength() || null == spanishNodeList.item(0).getTextContent() || spanishNodeList.item(0).getTextContent().isBlank()) {
+			if (0 != spanishNodeList.getLength() && spanishNodeList.item(0).getTextContent().contains(KEY)) {
+				toSetDirectly.put(name, spanishNodeList.item(0).getTextContent());
+			} else if (0 == spanishNodeList.getLength()
+					|| null == spanishNodeList.item(0).getTextContent()
+					|| spanishNodeList.item(0).getTextContent().isBlank()) {
 				Document englishDocument = builder.parse(new File(englishFileName));
 				XPath enXpath = XPathFactory.newInstance().newXPath();
 				NodeList enNodeList = (NodeList) enXpath.compile(find).evaluate(englishDocument, XPathConstants.NODESET);
-				if (0 == enNodeList.getLength() || null == enNodeList.item(0).getTextContent() || enNodeList.item(0).getTextContent().isBlank()) {
-					System.err.println(name + " has no equivalent in english file");
+				if (0 != spanishNodeList.getLength() && enNodeList.item(0).getTextContent().contains(KEY)) {
+					toSetDirectly.put(name, enNodeList.item(0).getTextContent());
+				} else if (0 == enNodeList.getLength() || null == enNodeList.item(0).getTextContent() || enNodeList.item(0).getTextContent().isBlank()) {
+					// warns that a name has no base in spanish nor in english
+					System.err.println(name + " has no equivalent in english nor spanish file");
 				} else {
 					en.put(name, enNodeList.item(0).getTextContent());
 				}
@@ -202,9 +303,9 @@ public class XmlParser {
 
 		try {
 			String find = "/root/data[@name='" + name + "']/value";
-		XPath translationXpath = XPathFactory.newInstance().newXPath();
-		NodeList translatedNodes = (NodeList) translationXpath.compile(find).evaluate(doc, XPathConstants.NODESET);
-		translatedNodes.item(0).setTextContent(translated);
+			XPath translationXpath = XPathFactory.newInstance().newXPath();
+			NodeList translatedNodes = (NodeList) translationXpath.compile(find).evaluate(doc, XPathConstants.NODESET);
+			translatedNodes.item(0).setTextContent(translated);
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
@@ -217,10 +318,6 @@ public class XmlParser {
 			throws TransformerException, UnsupportedEncodingException {
 
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-
-		// The default add many empty new line, not sure why?
-		// https://mkyong.com/java/pretty-print-xml-with-java-dom-and-xslt/
-		// Transformer transformer = transformerFactory.newTransformer();
 
 		// add a xslt to remove the extra newlines
 		Transformer transformer = transformerFactory.newTransformer(
@@ -260,8 +357,6 @@ public class XmlParser {
 				.map(TranslationResponse.Translation::getText)
 				.filter(Objects::nonNull)
 				.toList();
-		/*return request.getText().stream()
-				.toList();*/
 	}
 
 	private String getDataName(Node node) {
@@ -271,6 +366,11 @@ public class XmlParser {
 	}
 
 
+	/**
+	 * Quick and dirty AF. Gets the files found in target/
+	 * 
+	 * @return list of files
+	 */
 	private List<Path> getFiles() {
 		try {
 			File file = new File(XmlTranslatorApplication.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
